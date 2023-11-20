d6a9551ad2cb1999840d656a2dab2c4915b84f7e
berkescsaba <csaba.berkes@flowacademy.hu>
2023-11-20 15:03:45 +0100

    JSON-simple

--- projekt/src/Main.java
+++ projekt/src/Main.java
@@ -1,17 +1,47 @@
-import com.google.gson.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
 //        JsonWork.jsonPrint();
 
-        JsonWork.searchTeacher("János");
+//        JsonWork.searchTeacher("János");
 
 //        MenuSelect.mainMenu();
+
+        JSONObject main = new JSONObject();
+
+        JSONArray teacher = new JSONArray();
+        Map m = new LinkedHashMap(3);
+        m.put("firstName", "Nagy");
+        m.put("lastName", "Pista");
+        m.put("subject", "MATEK");
+
+        teacher.add(m);
+
+        main.put("teacher", teacher);
+
+
+        PrintWriter pw = new PrintWriter("JSONExample.json");
+        pw.write(teacher.toJSONString());
+
+        pw.flush();
+        pw.close();
+
     }
 }
 
